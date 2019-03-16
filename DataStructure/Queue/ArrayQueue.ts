4257d4c6a9a5902059e7acd6c7014c7768c39562
snayan <snayan@sina.com>
2019-03-16 21:22:48 +0800

    add:队列

---
+++ DataStructure/Queue/ArrayQueue.ts
@@ -0,0 +1,49 @@
+/**
+ * 使用数组实现的顺序队列
+ *
+ * @class ArrayQueue
+ * @template Item
+ */
+class ArrayQueue<Item> {
+  private values: Item[];
+
+  constructor() {
+    this.values = [];
+  }
+
+  /**
+   * 将当前元素入队列
+   *
+   * @param {Item} item
+   * @memberof ArrayQueue
+   */
+  public enqueue(item: Item) {
+    this.values.push(item);
+    return true;
+  }
+
+  /**
+   * 出队列，并返回
+   *
+   * @returns {(Item | null)} 队首的元素
+   * @memberof ArrayQueue
+   */
+  public dequeue(): Item | null {
+    if (this.values.length === 0) {
+      return null;
+    }
+    return this.values.shift();
+  }
+
+  /**
+   * 获取当前队列长度
+   *
+   * @returns {number} 当前队列长度
+   * @memberof ArrayQueue
+   */
+  public getQueueCount(): number {
+    return this.values.length;
+  }
+}
+
+export default ArrayQueue;
---
+++ DataStructure/Queue/LinkedQueue.ts
@@ -0,0 +1,58 @@
+import LinkedNode from '../LinkedNode';
+/**
+ * 使用链表实现链式队列
+ *
+ * @class LinkedQueue
+ * @template Item
+ */
+class LinkedQueue<Item> {
+  private head: LinkedNode;
+  private tail: LinkedNode;
+  private count: number;
+
+  constructor() {
+    this.head = new LinkedNode(null, null);
+    this.tail = this.head;
+    this.count = 0;
+  }
+
+  /**
+   * 将元素入队列
+   *
+   * @param {Item} item
+   * @memberof LinkedQueue
+   */
+  public enqueue(item: Item) {
+    const node = new LinkedNode(item, null);
+    this.tail.next = node;
+    this.tail = node;
+    this.count = this.count + 1;
+  }
+
+  /**
+   * 出队列
+   *
+   * @returns {(Item | null)} 返回队首元素
+   * @memberof LinkedQueue
+   */
+  public dequeue(): Item | null {
+    const node = this.head.next;
+    if (node !== null) {
+      this.head.next = node.next;
+      this.count = this.count - 1;
+    }
+    return node == null ? null : node.val;
+  }
+
+  /**
+   * 获取队列的长度
+   *
+   * @returns {number} 返回队列元素个数
+   * @memberof LinkedQueue
+   */
+  public getQueueCount(): number {
+    return this.count;
+  }
+}
+
+export default LinkedQueue;
