91236e3cf4a95890a514d8ba91478848e8661c50
snayan <snayan@sina.com>
2019-04-17 20:23:11 +0800

    fix: 循环和递归实现简单的二分查找

--- Algorithm/二分查找-循环和递归实现简单的二分查找/test.spec.ts
+++ Algorithm/二分查找-循环和递归实现简单的二分查找/test.spec.ts
@@ -25,6 +25,10 @@ describe('二分查找', () => {
       let arr = [];
       expect(binarySearch(arr, 1)).toBe(-1);
 
+      arr = [1];
+      expect(binarySearch(arr, 1)).toBe(0);
+      expect(binarySearch(arr, 2)).toBe(-1);
+
       // 常规数组
       arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
       for (let i = 0; i < arr.length; i++) {
