41a18a39797ca9d817330d6e4d4d707b48ebc271
snayan <snayan@sina.com>
2019-05-14 09:52:11 +0800

    fix: fix SkipList

--- DataStructure/SkipList/index.ts
+++ DataStructure/SkipList/index.ts
@@ -8,83 +8,103 @@ interface LinkedNode {
   data: number;
   maxLevel: number;
   nexts: LinkedNode[];
-  pres: LinkedNode[];
 }
+
+const MAX_LEVEL = 16; // 默认最大索引层级为16
 
 export default class SkipList {
   head: LinkedNode; // 头节点，哨兵节点，不存储实际数据
   maxLevel: number; // 索引最大层级
   count: number; // 存在节点个数
-  constructor(maxLevel: number) {
-    let emptyPointer = [];
-    for (let i = 0; i < maxLevel; i++) {
-      emptyPointer[i] = null;
+  constructor() {
+    let emptyNode = [];
+    for (let i = 0; i < MAX_LEVEL; i++) {
+      emptyNode[i] = null;
     }
     this.count = 0;
-    this.maxLevel = Math.max(maxLevel, 0);
-    this.head = { data: null, maxLevel: maxLevel, nexts: [...emptyPointer], pres: [...emptyPointer] };
+    this.maxLevel = 1;
+    this.head = { data: null, maxLevel: 0, nexts: emptyNode };
   }
 
   /* 生成一个随机的层索引数 */
   private randomLevel() {
-    return Math.floor(Math.random() * this.maxLevel);
+    return Math.floor(Math.random() * MAX_LEVEL + 1);
   }
 
-  /* 遍历索引 */
-  private scanWhere(condition: (node: LinkedNode) => boolean) {
+  public insert(value: number) {
+    // 获取随机索引层
+    const newLevel = this.randomLevel();
+
+    // 新节点
+    const newNode: LinkedNode = { data: value, maxLevel: newLevel, nexts: [] };
+    // 待更新节点
+    const updatedNode: LinkedNode[] = [];
+
     let p = this.head;
-    // 从顶层索引开始查找，i--，表示跳转到下一层索引
-    for (let i = this.maxLevel - 1; i > -1; i--) {
-      while (isNotEmpty(p.nexts[i]) && !condition(p.nexts[i])) {
-        p = p.nexts[i];
-      }
-    }
-    return p;
-  }
 
-  public add(value: number) {
-    if (this.find(value)) {
-      // 已经存在，直接返回
+    // 从生成的索引层开始查找
+    for (let level = newLevel - 1; level >= 0; level--) {
+      while (isNotEmpty(p.nexts[level]) && p.nexts[level].data < value) {
+        p = p.nexts[level];
+      }
+      // 跳转下一层之前，记录当前层需要更新的节点
+      updatedNode[level] = p;
+    }
+
+    // 如果跳表中已经存在value，则不插入
+    if (isNotEmpty(p.nexts[0]) && p.nexts[0].data === value) {
       return;
     }
-    // 获取随机索引层
-    const level = this.randomLevel();
-    // 新节点
-    const newNode = { data: value, maxLevel: level, nexts: [], pres: [] };
-    // 从原始链表层开始插入，逐层插入
-    for (let i = 0; i <= level; i++) {
-      let p = this.head;
-      while (isNotEmpty(p.nexts[i]) && p.nexts[i].data < value) {
-        p = p.nexts[i];
-      }
-      // 插入操作
-      newNode.nexts[i] = p.nexts[i];
-      newNode.pres[i] = p;
-      p.nexts[i] = newNode;
-      if (newNode.nexts[i]) {
-        newNode.nexts[i].pres[i] = newNode;
-      }
+
+    // 每层插入新的值
+    for (let i = 0; i < newLevel; i++) {
+      newNode.nexts[i] = updatedNode[i].nexts[i];
+      updatedNode[i].nexts[i] = newNode;
     }
+
+    // 更新maxLevel
+    if (newLevel > this.maxLevel) {
+      this.maxLevel = newLevel;
+    }
+
+    // count 值加1
     this.count++;
   }
 
-  public remove(value: number) {
-    const node = this.find(value);
-    if (!node) {
-      return;
+  public delete(value: number) {
+    let p = this.head;
+    const updatedNode: LinkedNode[] = [];
+
+    // 从顶层索引开始查找
+    for (let level = this.maxLevel - 1; level >= 0; level--) {
+      while (isNotEmpty(p.nexts[level]) && p.nexts[level].data < value) {
+        p = p.nexts[level];
+      }
+      updatedNode[level] = p;
     }
-    //  除了移除原始单链表，还需要移除索引上的该节点
-    for (let i = 0, level = node.maxLevel; i <= level; i++) {
-      node.pres[i].nexts[i] = node.nexts[i];
+
+    // 如果找到了，则开始删除
+    if (isNotEmpty(p.nexts[0]) && p.nexts[0].data === value) {
+      const level = p.nexts[0].maxLevel;
+      for (let i = 0; i < level; i++) {
+        p.nexts[i] = p.nexts[i].nexts[i];
+      }
+      // count 值减1
+      this.count--;
     }
-    this.count--;
   }
 
   public find(value: number) {
-    const node = this.scanWhere(node => node.data > value);
-    // 在原始单链表层查找
-    if (isNotEmpty(node) && node.data === value) {
-      return node;
+    let p = this.head;
+    // 从顶层索引开始查找，当level-1时，则表示跳转到下一层的索引中了
+    for (let level = this.maxLevel - 1; level >= 0; level--) {
+      while (isNotEmpty(p.nexts[level]) && p.nexts[level].data < value) {
+        p = p.nexts[level];
+      }
+    }
+    // 在原始单链表层判断
+    if (isNotEmpty(p.nexts[0]) && p.nexts[0].data === value) {
+      return p.nexts[0];
     }
 
     return null;
@@ -101,11 +121,8 @@ export default class SkipList {
     }
     for (let i = 0; i < this.maxLevel; i++) {
       let p = this.head;
-      let count = 0;
       msg += 'head';
       while (isNotEmpty(p.nexts[i])) {
-        count = p.nexts[i].data - (p.nexts[i].pres[i].data || 0);
-        // console.log(count)
         msg += ' ' + padLine(0) + ' ' + p.nexts[i].data;
         p = p.nexts[i];
       }
